c6f0f6c9e7c004d91bbc77fda8a12da02c56de5c
谭吉 <99425549@qq.com>
2019-11-27 14:38:42 +0800

    加了springcloud

---
+++ refueltwo-springcloud-parend01/src/main/java/com/refueltwo/main/RefueltwoSpringcloudParend01Application.java
@@ -0,0 +1,13 @@
+package com.refueltwo.main;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+public class RefueltwoSpringcloudParend01Application {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(RefueltwoSpringcloudParend01Application.class, args);
+    }
+
+}
