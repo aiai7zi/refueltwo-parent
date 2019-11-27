18e24988dfa01ba0f7da1939c1417100ff161417
谭吉 <99425549@qq.com>
2019-11-27 14:06:44 +0800

    添加了springboot工程

---
+++ refueltwo-springboot-parent01/src/main/java/com/refueltwo/main/RefueltwoSpringbootParent01Application.java
@@ -0,0 +1,13 @@
+package com.refueltwo.main;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+public class RefueltwoSpringbootParent01Application {
+
+    public static void main(String[] args) {
+       // SpringApplication.run(RefueltwoSpringbootParent01Application.class, args);
+    }
+
+}
